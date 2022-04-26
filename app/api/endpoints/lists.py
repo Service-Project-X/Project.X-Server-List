a618ef21304cbc59df35b49ccea3ef272ef4f5f5
asdf148 <wnduf0405_@naver.com>
2022-04-26 20:03:34 +0900

    [FEAT] List update function implementation

--- app/api/endpoints/lists.py
+++ app/api/endpoints/lists.py
@@ -2,18 +2,22 @@ from typing import List
 
 from fastapi import APIRouter
 
-from app import schemas
 from app.crud.crud_list import CRUDLists
-from app.schemas.list import ListCreate
+from app.schemas import list
 
 router = APIRouter()
 list_repository = CRUDLists()
 
-@router.get("/", response_model=List[schemas.list.List])
+@router.get("/", response_model=List[list.List])
 async def get_all():
     lists = list_repository.get_multiple()
     return lists
 
+@router.put("/{list_id}", response_model=list.List)
+async def update(list_id: str, update_list: list.ListUpdate):
+    updated_list = await list_repository.update(list_id, update_list)
+    return updated_list
+
 @router.post("/")
-async def create_list(create_list: ListCreate):
+async def create_list(create_list: list.ListCreate):
     return list_repository.create(create_list)
