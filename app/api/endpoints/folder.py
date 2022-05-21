b741507e74809e0a3f049be2391d2a5685df4e3e
asdf148 <wnduf0405_@naver.com>
2022-05-21 23:24:53 +0900

    [FEAT, FIX] Implement the move to the folder, create folder, delete folder API, Separate get_multiple in ChildFolder

--- app/api/endpoints/folder.py
+++ app/api/endpoints/folder.py
@@ -3,16 +3,24 @@ from typing import List
 from fastapi import APIRouter
 
 from app.crud.crud_folder import CRUDFolder
-from app.schemas import folder, child_folder, schedule, custom_response_type
+from app.crud.crud_schedule import CRUDSchedule
+from app.models.child_folder import ChildFolder
+from app.crud.crud_child_folder import CRUDChildFolder
+from app.models.schedule import Schedule
+from app.schemas import folder, custom_response_type
 
 router = APIRouter()
 folder_repository = CRUDFolder()
+child_folder_repository = CRUDChildFolder()
+schedule_repository = CRUDSchedule()
 
 @router.get("/{folder_id}", response_model=custom_response_type.CustomResponseType)
-async def get_all(folder_id: str):
-    in_folders = folder_repository.get_multiple(folder_id)
+async def get_all(folder_id: int) -> [ChildFolder or Schedule]:
+    child_folders: List[ChildFolder] = child_folder_repository.get_multiple_in_folder(folder_id)
+    schedules: List[Schedule] = schedule_repository.get_multiple_in_folder(folder_id)
+
     new_custom_response_type = custom_response_type.CustomResponseType
-    new_custom_response_type.data = in_folders
+    new_custom_response_type.data = [child_folders, schedules]
     return new_custom_response_type
 
 @router.post("/", response_model=custom_response_type.CustomResponseType)
@@ -22,15 +30,16 @@ async def create_list(create_folder: folder.FolderCreate):
     new_custom_response_type.data = created_folder
     return new_custom_response_type
 
-@router.put("/{folder_id}", response_model=custom_response_type.CustomResponseType)
-async def update(folder_id: str, update_folder: folder.FolderUpdate):
-    updated_folder = folder_repository.update(folder_id, update_folder)
-    new_custom_response_type = custom_response_type.CustomResponseType
-    new_custom_response_type.data = updated_folder
-    return new_custom_response_type
+# 필요할까?
+# @router.put("/{folder_id}", response_model=custom_response_type.CustomResponseType)
+# async def update(folder_id: int, update_folder: folder.FolderUpdate):
+#     updated_folder = folder_repository.update(folder_id, update_folder)
+#     new_custom_response_type = custom_response_type.CustomResponseType
+#     new_custom_response_type.data = updated_folder
+#     return new_custom_response_type
 
 @router.delete("/{folder_id}", response_model=custom_response_type.CustomResponseType)
-async def delete(folder_id: str):
+async def delete(folder_id: int):
     deleted_folder = folder_repository.delete(folder_id)
     new_custom_response_type = custom_response_type.CustomResponseType
     new_custom_response_type.message = deleted_folder
